0af2719710b3d308b7b3e4fc7f502444147375a1
larrysmith <larrypsmith415@gmail.com>
2020-04-10 09:41:11 -0700

    Add projects info

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
+
+export default () => (
+  <AppBar position="static">
+    <Toolbar>
+      <Typography variant="h6">
+        Larry Smith
+      </Typography>
+    </Toolbar>
+  </AppBar>
+)
---
+++ src/components/ProjectList.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  Grid,
+  Typography
+} from '@material-ui/core'
+
+export default () => (
+  <React.Fragment>
+    <Typography variant="h5">
+      Take a look at my projects.
+    </Typography>
+    <Grid>
+      
+    </Grid>
+  </React.Fragment>
+)

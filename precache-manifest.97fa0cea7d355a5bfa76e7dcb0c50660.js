da297077864aa28772f328f8abf95d41e3c31d7d
larrysmith <larrypsmith415@gmail.com>
2020-04-20 11:54:41 -0700

    Updates

---
+++ precache-manifest.97fa0cea7d355a5bfa76e7dcb0c50660.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06ed30c31ca73c3d54f02819c4a36b64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d997abbe3eb4e38db13a",
+    "url": "/static/css/main.b008dc90.chunk.css"
+  },
+  {
+    "revision": "461d0e23a57936cc73fc",
+    "url": "/static/js/2.ee4f56bd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.ee4f56bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d997abbe3eb4e38db13a",
+    "url": "/static/js/main.4ad1973f.chunk.js"
+  },
+  {
+    "revision": "30ef98ccc078dcdc8699",
+    "url": "/static/js/runtime-main.13135219.js"
+  },
+  {
+    "revision": "7955a900dba6ca12d5f412e95a73748a",
+    "url": "/static/media/bitter.7955a900.png"
+  },
+  {
+    "revision": "a7d0a402977907bdfbb3016872cb2558",
+    "url": "/static/media/golden-gate.a7d0a402.jpg"
+  },
+  {
+    "revision": "a9ead9e93b72e734c90db5d587efeadb",
+    "url": "/static/media/plotify.a9ead9e9.png"
+  },
+  {
+    "revision": "fb2e67c8311f8a0b7916f136163a9eee",
+    "url": "/static/media/thought-board.fb2e67c8.png"
+  }
+]);
